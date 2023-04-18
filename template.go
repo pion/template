b45d1a8387c44819d677ac52838ea5aa91bed8f8
Steffen Vogel <post@steffenvogel.de>
2023-04-18 13:59:47 +0200

    Make package REUSE compliant

--- template.go
+++ template.go
@@ -1,2 +1,5 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // Package template implements an example package in the Pion orgnaization
 package template
--- template_test.go
+++ template_test.go
@@ -1,2 +1,5 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // Package template_test implements unit tests for the template package
 package template_test
